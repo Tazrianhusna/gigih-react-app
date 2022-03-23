eaffddb491d7c906aed2e3d4d2f1d0b13985a659
Tazrianhusna <tazrianhusna11@gmail.com>
2022-03-23 12:06:32 +0700

    homework mod2 s1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './dataAlbum/single-sample.js';
 
 const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="playlist-form">
+      <img src = {data.album.images[1].url}></img>
+      <h1>{data.name}</h1>
+      <h3>{data.artists[0].name} - {data.album.name}</h3>
+      <button className="btn btn-primary">PLAY</button>
+      </div>
     </div>
   );
 }
