2afecc33f4d87cca8ac3ec0341d634a23a755cc1
Tazrianhusna <tazrianhusna11@gmail.com>
2022-04-06 02:36:17 +0700

    memindahkan access token state ke redux store (homework mod4 s1)

--- src/component/feature/Navbar.js
+++ src/component/feature/Navbar.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { useTokenContext } from '../context/Token'
 import { clearStorage } from '../../utility/storage'
 
 import './css/Navbar.css'
 import { login, logout } from '../../utility/auth'
+import { useSelector } from 'react-redux'
 
 function Navbar() {
-  const { token } = useTokenContext()
+  const token = useSelector((state) => state.token.value)
 
   window.addEventListener("beforeunload", () => clearStorage())
 
   return (
     <div className="navbar">
       <div className="navbar-logo">
+        <h2>myspotify</h2>
       </div>
       <div className="navbar-nav">
         {token ? (
           <button onClick={logout} className="btn-logout">Logout</button>
         ) : (
-          <button onClick={login} className="btn-login">Login to Spotify</button>
+          <button onClick={login} className="btn-login">Login</button>
         )}
       </div>
     </div>
