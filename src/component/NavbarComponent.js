76769f9a7202703d2c6525f7da59912bb4473da7
Tazrianhusna <tazrianhusna11@gmail.com>
2022-04-11 08:00:34 +0700

    membersihkan code menggunakan eslint/prettier

--- src/component/NavbarComponent.js
+++ src/component/NavbarComponent.js
@@ -1,26 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Navbar, Container, Nav, Button } from 'react-bootstrap'
-import { logout } from '../utils/auth'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { logout } from '../utils/auth';
 
 function NavbarComponent() {
   return (
-    <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar
+      sticky="top"
+      collapseOnSelect
+      expand="lg"
+      bg="secondary"
+      variant="dark"
+    >
       <Container>
         <Navbar.Brand>Myspotify</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className="mx-auto">
           <Nav className="me-auto text-center">
-            <Nav.Link as={NavLink} to="/myplaylist">My Playlist</Nav.Link>
-            <Nav.Link as={NavLink} to="/create">Create Playlist</Nav.Link>
+            <Nav.Link as={NavLink} to="/myplaylist">
+              My Playlist
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/create">
+              Create Playlist
+            </Nav.Link>
           </Nav>
           <Nav>
-            <Button variant="danger" onClick={logout}>Logout</Button>
+            <Button variant="danger" onClick={logout}>
+              Logout
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 
-export default NavbarComponent
+export default NavbarComponent;
